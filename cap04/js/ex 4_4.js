b27282ecc0e649f8c6a926827f5c282400940c9c
lepp <lepplogin@gmail.com>
2026-07-09 23:33:12 -0300

    ..

--- cap04/js/ex 4_4.js
+++ cap04/js/ex 4_4.js
@@ -1,2 +1,13 @@
 const frm = document.querySelector('form')
 const resp = document.querySelector('h2')
+
+frm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const hora = Number(frm.inHora.value)
+    let horaFranca = hora + 5
+
+    if(horaFranca > 24 ){ // se passar das 24 hrs na frança 
+        horaFranca = horaFranca - 24  //... subtrai 24 , reseta 
+    }
+    resp.innerHTML = `${horaFranca.toFixed(2)}`
+})
