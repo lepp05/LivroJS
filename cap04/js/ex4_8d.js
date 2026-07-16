8d4db208b0ac83afda9df2b0d2155d44bad2f486
lepp <lepplogin@gmail.com>
2026-07-15 23:45:09 -0300

    ..

---
+++ cap04/js/ex4_8d.js
@@ -0,0 +1,28 @@
+const frm = document.querySelector('form')
+const resp1 = document.getElementById('resp1')
+const resp2 = document.getElementById('resp2')
+
+frm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const ladoA = Number(frm.inLadoA.value)
+    const ladoB = Number(frm.inLadoB.value)
+    const ladoC = Number(frm.inLadoC.value)
+
+    if(ladoA > (ladoB + ladoC) || ladoB > (ladoA + ladoC) || ladoC > (ladoB + ladoA)){
+        resp1.innerHTML = 'Lados não podem formar um triangulo'
+        resp2.innerHTML = ''
+        return
+
+    }else{
+        resp1.innerHTML = 'Lados podem formar um triangulo'
+    }
+
+    if(ladoA == ladoB && ladoB == ladoC){
+        resp2.innerHTML = 'Equilátero'
+    }else if(ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+        resp2.innerHTML = 'Isósceles'
+    }else {
+        resp2.innerHTML = 'Escaleno'
+    }
+})
