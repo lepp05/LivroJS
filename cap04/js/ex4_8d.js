95d88946b40f90d2362d6c3baf76f22df3919fb7
lepp <lepplogin@gmail.com>
2026-07-15 23:47:05 -0300

    ..

--- cap04/js/ex4_8d.js
+++ cap04/js/ex4_8d.js
@@ -9,7 +9,7 @@ frm.addEventListener('submit', (e) =>{
     const ladoB = Number(frm.inLadoB.value)
     const ladoC = Number(frm.inLadoC.value)
 
-    if(ladoA > (ladoB + ladoC) || ladoB > (ladoA + ladoC) || ladoC > (ladoB + ladoA)){
+    if(ladoA > (ladoB + ladoC) || ladoB > (ladoA + ladoC) || ladoC > (ladoB + ladoA)){  // se um lado for maior que a soma dos outros 2, nao pode formar  
         resp1.innerHTML = 'Lados não podem formar um triangulo'
         resp2.innerHTML = ''
         return
@@ -18,11 +18,11 @@ frm.addEventListener('submit', (e) =>{
         resp1.innerHTML = 'Lados podem formar um triangulo'
     }
 
-    if(ladoA == ladoB && ladoB == ladoC){
+    if(ladoA == ladoB && ladoB == ladoC){  // se forem iguais
         resp2.innerHTML = 'Equilátero'
-    }else if(ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+    }else if(ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) { // se 2 lados forem iguais
         resp2.innerHTML = 'Isósceles'
     }else {
-        resp2.innerHTML = 'Escaleno'
+        resp2.innerHTML = 'Escaleno'   // se todos os lados forem diferentes
     }
 })
